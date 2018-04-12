9469bcb5866ec33930d2f156870c8c132810f2bc
U-CRITEOIS\t.clavet <t.clavet@LFW99QH2.criteois.lan>
2018-04-12 13:11:59 +0200

    up to 18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,23 @@
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i++;
+	}
+}
