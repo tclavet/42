30b0e352a6a7eb7afa1b3442cd56067dd52a3424
U-CRITEOIS\t.clavet <t.clavet@LFW99QH2.criteois.lan>
2018-04-12 21:13:18 +0200

    Until ex23

---
+++ ex21/ft_range.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*tab;
+	int	i;
+
+	if (min >= max)
+		return(NULL);
+	tab = malloc(sizeof(*tab) * (max - min);
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	return (tab);
+}
